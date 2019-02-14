0324ad343fbc53ef5566071287be60444239483d
Joe Berkem <jberkem@gmail.com>
2019-02-14 17:24:07 +0000

    registration working

--- register.php
+++ register.php
@@ -1,8 +1,17 @@
 <?php 
 include('db_server.php');
 
-// $query = "INSERT INTO users (email, password) VALUES ('$clean_email', '$clean_password');";
+if (!empty($_POST)) {
 
+//the submitted login details are assigned to variables 
+	$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
+	$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
+	$password = isset($_POST['password']) ? $_POST['password'] : null;
+	$email = isset($_POST['email']) ? $_POST['email'] : null;
+
+//log input to the database
+	$result = registerNewUser ($email, $password, $firstname, $lastname);
+}
 
 ?>
 
@@ -16,7 +25,7 @@ include('db_server.php');
 
 	<h1>Register For An Account</h1>
 
-	<form action="login.php" method="post">
+	<form action="register.php" method="post">
 		
 		<div class="registernameinput">
 			<div class="registerfirstnameinput">
@@ -32,7 +41,7 @@ include('db_server.php');
 		<label id="email">Email*</label>
 		<input type="email" name="email" placeholder="Email Address">
 
-		<label id="password">Set A Password*</label>
+		<label id="password">Password*</label>
 		<input type="text" name="password" placeholder="Set A Password">
 
 		<input class="bigbutton" type="submit" name="action" value="Register">
